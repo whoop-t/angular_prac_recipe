43424212319a129f81a58d360ad353df42607085
Tommy Mallis <tommymallis@gmail.com>
2020-02-22 15:06:29 -0800

    Changed shoppping-list emitter to Subject, changed emits to nexty

--- src/app/recipe-book/recipe-book.component.ts
+++ src/app/recipe-book/recipe-book.component.ts
@@ -1,21 +1,15 @@
 import { Component, OnInit } from "@angular/core";
 import { Recipe } from "./recipe.model";
-import { RecipeService } from "./recipe.service";
 
 @Component({
   selector: "app-recipe-book",
   templateUrl: "./recipe-book.component.html",
-  styleUrls: ["./recipe-book.component.css"],
-  providers: [RecipeService]
+  styleUrls: ["./recipe-book.component.css"]
 })
 export class RecipeBookComponent implements OnInit {
   selectedRecipeDetails: Recipe;
 
-  constructor(private recipeService: RecipeService) {}
+  constructor() {}
 
-  ngOnInit() {
-    this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
-      this.selectedRecipeDetails = recipe;
-    });
-  }
+  ngOnInit() {}
 }
